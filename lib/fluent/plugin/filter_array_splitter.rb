bcab59d42e381f25fa644511e6ce9054d9c63a63
pcoffmanjr <pcoffman@ctc-america.com>
2023-10-16 22:22:40 +0000

    Implement array splitting functionality and add tests

--- lib/fluent/plugin/filter_array_splitter.rb
+++ lib/fluent/plugin/filter_array_splitter.rb
@@ -7,8 +7,36 @@ module Fluent
     class ArraySplitterFilter < Fluent::Plugin::Filter
       Fluent::Plugin.register_filter("array_splitter", self)
 
-      def filter(tag, time, record)
+      def configure(conf)
+        super
+        log.info "Configuring Array Splitter Filter"
+      end
+
+      def filter_stream(tag, es)
+        new_es = Fluent::MultiEventStream.new
+        es.each do |time, record|
+          if record['message'].is_a?(Array)
+            record['message'].each do |value|
+              new_record = record.dup
+              new_record['message'] = value
+              new_es.add(time, new_record)
+            end
+          elsif record['target_field'].is_a?(Array)
+            record['target_field'].each do |hash|
+              hash.each do |k, v|
+                new_record = record.dup
+                new_record.delete('target_field')
+                new_record[k] = v
+                new_es.add(time, new_record)
+              end
+            end
+          else
+            new_es.add(time, record)
+          end
+        end
+        new_es
       end
     end
   end
 end
+
--- test/plugin/test_filter_array_splitter.rb
+++ test/plugin/test_filter_array_splitter.rb
@@ -4,15 +4,32 @@ require "fluent/plugin/filter_array_splitter.rb"
 class ArraySplitterFilterTest < Test::Unit::TestCase
   setup do
     Fluent::Test.setup
+    @driver = create_driver
   end
 
-  test "failure" do
-    flunk
+  def create_driver(conf = "")
+    Fluent::Test::Driver::Filter.new(Fluent::Plugin::ArraySplitterFilter).configure(Fluent::Config.parse(conf, "(test)", "(test_dir)", true))
   end
 
-  private
+  sub_test_case "filter" do
+    test "expand array in message field" do
+      record = {
+        "key1" => "hoge",
+        "key2" => "foo",
+        "key3" => "bar",
+        "message" => ["v1", "v2", "v3"]
+      }
+      @driver.run(default_tag: "test") do
+        @driver.feed(record)
+      end
+      filtered_records = @driver.filtered_records
+      assert_equal(3, filtered_records.size)
+      assert_equal("v1", filtered_records[0]["message"])
+      assert_equal("v2", filtered_records[1]["message"])
+      assert_equal("v3", filtered_records[2]["message"])
+    end
 
-  def create_driver(conf)
-    Fluent::Test::Driver::Filter.new(Fluent::Plugin::ArraySplitterFilter).configure(conf)
+    # More tests for other cases...
   end
 end
+
