4a8e44cd67e83f58c42a394dac52086a44017f39
pcoffmanjr <pcoffman@ctc-america.com>
2023-10-20 14:45:23 +0000

    Added key_name config and updated to version 0.1.3

--- lib/fluent/plugin/filter_array_splitter.rb
+++ lib/fluent/plugin/filter_array_splitter.rb
@@ -10,6 +10,9 @@ module Fluent
       desc 'The field name to split and expand array values'
       config_param :array_key, :string, default: 'message'
 
+      desc 'The field name to rename the key of a single array element'
+      config_param :key_name, :string, default: nil
+
       def configure(conf)
         super
         raise Fluent::ConfigError, "'array_key' parameter is required" unless conf.has_key?('array_key')
@@ -18,27 +21,28 @@ module Fluent
 
       def filter_stream(tag, es)
         new_es = Fluent::MultiEventStream.new
+
         es.each do |time, record|
           if record[@array_key].is_a?(Array)
             record[@array_key].each do |value|
               new_record = record.dup
               new_record.delete(@array_key)
+
               if value.is_a?(Hash)
-                value.each do |k, v|
-                  new_record[k] = v
-                end
+                value.each { |k, v| new_record[k] = v }
               else
-                new_record[@array_key] = value
+                new_record[@key_name.nil? ? @array_key : @key_name] = value
               end
+
               new_es.add(time, new_record)
             end
           else
             new_es.add(time, record)
           end
         end
+
         new_es
       end
     end
   end
 end
-
